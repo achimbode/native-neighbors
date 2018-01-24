6c491181b4b02efa43952c79025536a3cd61a834
Andrea Ceccarelli <gltch2003@gmail.com>
2018-01-24 13:20:41 +0100

    Test: unit test render empty and long message on homepage

--- containers/__tests__/chatMessages-test.js
+++ containers/__tests__/chatMessages-test.js
@@ -45,3 +45,84 @@ const dummyProps = {
     }
   ]
 }
+
+
+describe('Empty Messages', () => {
+  it('should render correctly', () => {
+    const output = shallow(
+      <Messages
+        username={emptyProps.username}
+        color={emptyProps.color}
+        users={emptyProps.users}
+        messages={emptyProps.messages} />
+    );
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+}); 
+
+const emptyProps = {
+  username: 'Andrea',
+  color: 'fuchsia',
+  users: {
+
+    'Andrea': {
+      avatar_color: 'navy'
+    }
+    ,
+    'Achim': {
+      avatar_color: 'navy'
+    },
+  },
+  messages: [
+    {
+      from: 'Andrea',
+      msg: '',
+    },
+    {
+      from: 'Achim',
+      msg: '',
+    }
+  ]
+}
+
+
+describe('Very Long Messages', () => {
+  it('should render correctly', () => {
+    const output = shallow(
+      <Messages
+        username={fullProps.username}
+        color={fullProps.color}
+        users={fullProps.users}
+        messages={fullProps.messages} />
+    );
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+});
+
+const fullProps = {
+  username: 'Andrea',
+  color: 'fuchsia',
+  users: {
+
+    'Andrea': {
+      avatar_color: 'navy'
+    }
+    ,
+    'Achim': {
+      avatar_color: 'navy'
+    },
+  },
+  messages: [
+    {
+      from: 'Andrea',
+      msg: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    },
+    {
+      from: 'Achim',
+      msg: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    }
+  ]
+}
+
+
+
